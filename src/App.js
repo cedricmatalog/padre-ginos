132913439c1eff1bb85301daeac8a082c5cfe9a3
cedric <cedricmatalog@gmail.com>
2025-07-11 20:30:24 +0800

    Add Pizza component and pizza data for menu display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.title),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const pizzaData = [
+  {
+    title: "Margherita",
+    description: "Classic pizza with tomato sauce and mozzarella",
+  },
+  {
+    title: "Pepperoni",
+    description: "Spicy pepperoni with mozzarella and tomato sauce",
+  },
+  {
+    title: "Vegetarian",
+    description: "Loaded with fresh vegetables and mozzarella",
+  },
+  {
+    title: "BBQ Chicken",
+    description: "Grilled chicken with BBQ sauce and red onions",
+  },
+  {
+    title: "Hawaiian",
+    description: "Ham and pineapple with mozzarella",
+  },
+];
+
 const App = () => {
   return React.createElement(
     "div",
     null,
-    React.createElement("h1", {}, "Padre Gino's")
+    React.createElement("h1", {}, "Padre Gino's"),
+    [
+      React.createElement(Pizza, {
+        title: "Margherita",
+        description: "Classic pizza with tomato sauce and mozzarella",
+      }),
+      React.createElement(Pizza, {
+        title: "Pepperoni",
+        description: "Spicy pepperoni with mozzarella and tomato sauce",
+      }),
+      React.createElement(Pizza, {
+        title: "Vegetarian",
+        description: "Loaded with fresh vegetables and mozzarella",
+      }),
+      React.createElement(Pizza, {
+        title: "BBQ Chicken",
+        description: "Grilled chicken with BBQ sauce and red onions",
+      }),
+      React.createElement(Pizza, {
+        title: "Hawaiian",
+        description: "Ham and pineapple with mozzarella",
+      }),
+    ]
   );
 };
 
